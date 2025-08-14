92be67b2ee37de271b602c4fd395c16d6d9232ed
AnddyPest <mauripestchanker@gmail.com>
2025-08-14 17:23:49 -0300

    cosillas

--- project/Models/Propietarios.cs
+++ project/Models/Propietarios.cs
@@ -10,18 +10,21 @@ namespace project.Models
         [Key]
         [Display(Name = "Código Interno")]
         public int PropietarioId { get; set; }
-        [Required]
+        [Required, StringLength(100)]
         public string Nombre { get; set; }
-        [Required]
+        [Required, StringLength(100)]
         public string Apellido { get; set; }
-        [Required]
+        [Required, StringLength(10)]
         public string Dni { get; set; }
-        [Required]
+        [Required, Phone]
         [Display(Name = "Teléfono")]
+        [StringLength(15)]
         public string Telefono { get; set; }
         [Required, EmailAddress]
+        [StringLength(200)]
         public string Email { get; set; }
-        [Required(ErrorMessage = "La clave es obligatoria"), DataType(DataType.Password)]
+        //USAR HASH PARA ENVIAR A LA BD - HASHEAR AL MOMENTO DE GUARDAR EN REPO
+        [Required(ErrorMessage = "La clave es obligatoria"), DataType(DataType.Password), MinLength(8)]
         public string Clave { get; set; }
 
         public override string ToString()
