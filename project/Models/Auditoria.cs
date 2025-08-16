e3f25a2e59907316963b40bb969db84808fbe16d
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2025-08-16 17:41:41 -0300

    [model]: Add auditoria

---
+++ project/Models/Auditoria.cs
@@ -0,0 +1,21 @@
+﻿namespace project.Models
+{
+    public class Auditoria
+    {
+
+        public int IdAuditoria {  get; set; }
+        public int IdUsuario { get; set; }
+        public int? IdContrato { get; set; }
+        public int? IdPago { get; set; }
+        public DateTime fecha { get; set; } = DateTime.Now;
+        public string motivo { get; set; }
+
+        public Auditoria(int idUsuario, int? idContrato, int? idPago, string motivo)
+        {
+            this.IdUsuario = idUsuario;
+            this.IdContrato = idContrato;
+            this.IdPago = idPago;
+            this.motivo = motivo;
+        }
+    }
+}
