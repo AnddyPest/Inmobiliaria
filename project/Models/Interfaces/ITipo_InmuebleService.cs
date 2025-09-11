0ad18868aac603c44f07a389466f6a6fde53bf12
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2025-09-11 18:42:52 -0300

    [feat]: Gestion de tipos de inmueble listo

--- project/Models/Interfaces/ITipo_InmuebleService.cs
+++ project/Models/Interfaces/ITipo_InmuebleService.cs
@@ -3,7 +3,7 @@
     public interface ITipo_InmuebleService
     {
         public Task<(string?, List<Tipo_Inmueble>?)> getAllTipoInmueble();
-
+        public Task<(string?, bool)> ValidarQueNoEsteAsignado(int id_tipo_inmueble);
         public Task<(string?, List<Tipo_Inmueble>?)> getAllTipoInmueble(int nroPagina, int cantidadPaginasPorHoja);
         public Task<(string?, bool)> createTipoInmueble(Tipo_Inmueble tipo_Inmueble);
         public Task<(string?, bool)> updateTipoInmueble(Tipo_Inmueble tipo_Inmueble);
