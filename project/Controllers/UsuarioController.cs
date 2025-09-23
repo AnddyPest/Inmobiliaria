14193f5afa836dd61ef24bf6abb6a6a46dcacd9e
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2025-09-22 22:44:27 -0300

    [feat]: Administrador puede resetear contraseñas

--- project/Controllers/UsuarioController.cs
+++ project/Controllers/UsuarioController.cs
@@ -26,9 +26,21 @@ public class UsuarioController : Controller
     {
         if (!ModelState.IsValid) return BadRequest(ModelState);
         int rolACambiar = idRolActual == 1 ? 2 : 1;
-        if(User.FindFirst("idUsuario")!.Value == idUsuario.ToString()) return BadRequest("No puedes cambiar tu propio rol");
+        if (User.FindFirst("idUsuario")!.Value == idUsuario.ToString()) return BadRequest("No puedes cambiar tu propio rol");
         (string?, bool) result = await _usuarioService.CambiarRol(idUsuario, rolACambiar);
-        if(result.Item1 != null && !result.Item2) return BadRequest(result.Item1);
+        if (result.Item1 != null && !result.Item2) return BadRequest(result.Item1);
+        return Ok(result);
+    }
+    [HttpGet("Usuario/resetearContrasena")]
+    public async Task<IActionResult> ResetearContrasena(int idUsuario) // , int idRolActual Agregar esto si es necesario
+    {
+        System.Console.WriteLine("Linea33");
+        if (!ModelState.IsValid) return BadRequest("No se pudo resetear la contraseña");
+        System.Console.WriteLine("Linea38");
+        //if(User.FindFirst("idUsuario")!.Value != idUsuario.ToString() && idRolActual == 1) return BadRequest("No puedes resetear la contraseña de otro administrador"); Pensar si es necesario
+        (string?, bool) result = await _usuarioService.resetearContraseña(idUsuario);
+        if (result.Item1 != null && !result.Item2) return BadRequest(result.Item1);
+        System.Console.WriteLine("Linea43");
         return Ok(result);
     }
 }
