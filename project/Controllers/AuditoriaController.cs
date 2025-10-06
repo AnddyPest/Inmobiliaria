3143859bb6f73cda447d64505301d284e998a705
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2025-10-06 19:13:10 -0300

    Database

--- project/Controllers/AuditoriaController.cs
+++ project/Controllers/AuditoriaController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using project.Models;
 namespace project.Controllers
 {
-    [Authorize]
+    [Authorize (Roles = "Administrador")]
     public class AuditoriaController : Controller
     {
         private IAuditoriaService _auditoriaService;
